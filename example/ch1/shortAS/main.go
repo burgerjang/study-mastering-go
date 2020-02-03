ed4120407bbad685fa83e464ac982ee66d7ddced
burgerjang <rozen8831@gmail.com>
2020-02-03 06:55:30 +0000

    Add example shortAS

--- example/ch1/shortAS/main.go
+++ example/ch1/shortAS/main.go
@@ -8,9 +8,12 @@ func main() {
 	foo := 123
 	fmt.Println(foo)
 
-	bar = 234
-	fmt.Println(bar)
+//	bar = 234
+//	fmt.Println(bar)
 
 	bar := 234
+	fmt.Println(bar)
+
+	bar = 345
 	fmt.Println(bar)
 }
