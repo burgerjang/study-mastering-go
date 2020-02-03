807524f0d9ac3e69d60d4da070c9f7d179bc1366
burgerjang <rozen8831@gmail.com>
2020-02-03 06:40:00 +0000

    Rollback original

--- example/ch1/stdOUT/stdOUT.go
+++ example/ch1/stdOUT/stdOUT.go
@@ -11,7 +11,7 @@ func main() {
 	if len(arguments) == 1 {
 		myString = "Please give me one argument!"
 	} else {
-		myString = arguments[0]
+		myString = arguments[1]
 	}
 
 	io.WriteString(os.Stdout, myString)
